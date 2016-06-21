b453e6a803efb53ae3702d85b09b4a7212dc5beb
Matthew Grogan <mvgnyc@gmail.com>
2016-06-21 16:05:50 -0400

    Changed the str format

--- liberty_bell/spin_result.py
+++ liberty_bell/spin_result.py
@@ -6,3 +6,12 @@ class Spin_Result(object):
 
         self.reels = reels
         self.winner_paid = winner_paid
+
+    def __str__(self):
+        """ Convert to a string """
+
+        result = "Payout: %i [" % self.winner_paid
+        result += ' | '.join(str(r) for r in self.reels)
+        result += " ]"
+
+        return result

719562ab312f35e1a87718f64684917731a5b172
Matthew Grogan <mvgnyc@gmail.com>
2016-06-21 14:37:30 -0400

    Finished payouts

--- liberty_bell/payout.py
+++ liberty_bell/payout.py
@@ -20,6 +20,93 @@ class Liberty_Bell_Payout(object):
         return all(self.symbols[i] == candidate[i] for i in range(len(candidate)))
 
 
+class Heart_Payout(object):
+    """ Three Hearts """
+
+    def __init__(self):
+        """ Initialize the payout """
+
+        self.symbols = [symbols.HEART,
+                        symbols.HEART,
+                        symbols.HEART]
+        self.winner_paid = 16
+
+    def is_match(self, candidate):
+        """ Check if symbols is a match """
+
+        return all(self.symbols[i] == candidate[i] for i in range(len(candidate)))
+
+
+class Diamond_Payout(object):
+    """ Three Diamonds """
+
+    def __init__(self):
+        """ Initialize the payout """
+
+        self.symbols = [symbols.DIAMOND,
+                        symbols.DIAMOND,
+                        symbols.DIAMOND]
+        self.winner_paid = 12
+
+    def is_match(self, candidate):
+        """ Check if symbols is a match """
+
+        return all(self.symbols[i] == candidate[i] for i in range(len(candidate)))
+
+
+class Spade_Payout(object):
+    """ Three Spades """
+
+    def __init__(self):
+        """ Initialize the payout """
+
+        self.symbols = [symbols.SPADE,
+                        symbols.SPADE,
+                        symbols.SPADE]
+        self.winner_paid = 8
+
+    def is_match(self, candidate):
+        """ Check if symbols is a match """
+
+        return all(self.symbols[i] == candidate[i] for i in range(len(candidate)))
+
+
+class Horseshoe_Star_Payout(object):
+    """ Two horseshoes and one star """
+
+    def __init__(self):
+        """ Initialize the payout """
+
+        self.symbols = [symbols.HORSESHOE,
+                        symbols.HORSESHOE,
+                        symbols.STAR]
+        self.winner_paid = 4
+
+    def is_match(self, candidate):
+        """ Check if symbols is a match """
+
+        return all(sorted(self.symbols)[i] == sorted(candidate)[i] for i in range(len(candidate)))
+
+class Horseshoe_Payout(object):
+    """ Two horseshoes """
+
+    def __init__(self):
+        """ Initialize the payout """
+
+        self.symbol = symbols.HORSESHOE
+        self.winner_paid = 2
+
+    def is_match(self, candidate):
+        """ Check if symbols is a match """
+
+        count = 0
+
+        for i in range(len(candidate)):
+            if candidate[i] == self.symbol:
+                count = count + 1
+
+        return count >= 2
+
 class Liberty_Bell_Payout_Table(object):
     """ Class to hold the payout table """
 
@@ -27,7 +114,9 @@ class Liberty_Bell_Payout_Table(object):
         """ Initialize the payout table """
 
         self.name = "Liberty Bell Payout Table"
-        self.payouts = [Liberty_Bell_Payout()]
+        self.payouts = [Liberty_Bell_Payout(), Heart_Payout(),
+            Diamond_Payout(), Spade_Payout(), Horseshoe_Star_Payout(),
+            Horseshoe_Payout()]
 
     def calculate_payout(self, symbols):
         """ Check if symbols is a winner! """
--- liberty_bell/test/test_liberty_bell.py
+++ liberty_bell/test/test_liberty_bell.py
@@ -5,6 +5,7 @@ import liberty_bell
 
 symbols = liberty_bell.Liberty_Bell_Symbols()
 
+
 def test_reel():
 
     # Create the reel
@@ -133,14 +134,94 @@ def test_payout():
     # Check payout
     #assert spin_result.winner_paid == 20
 
+
 def test_payout_table():
 
     payout_table = liberty_bell.Liberty_Bell_Payout_Table()
 
+    # Jackpot
     spin_result = [symbols.LIBERTY_BELL,
-                    symbols.LIBERTY_BELL,
-                    symbols.LIBERTY_BELL]
+                   symbols.LIBERTY_BELL,
+                   symbols.LIBERTY_BELL]
 
     result = payout_table.calculate_payout(spin_result)
 
     assert result == 20
+
+    # no winner
+    spin_result = [symbols.STAR,
+                   symbols.LIBERTY_BELL,
+                   symbols.LIBERTY_BELL]
+
+    result = payout_table.calculate_payout(spin_result)
+
+    assert result == 0
+
+    # Hearts
+    spin_result = [symbols.HEART,
+                   symbols.HEART,
+                   symbols.HEART]
+
+    result = payout_table.calculate_payout(spin_result)
+
+    assert result == 16
+
+    # Diamond
+    spin_result = [symbols.DIAMOND,
+                   symbols.DIAMOND,
+                   symbols.DIAMOND]
+
+    result = payout_table.calculate_payout(spin_result)
+
+    assert result == 12
+
+    # Spade
+    spin_result = [symbols.SPADE,
+                   symbols.SPADE,
+                   symbols.SPADE]
+
+    result = payout_table.calculate_payout(spin_result)
+
+    assert result == 8
+
+    # Horseshoe + star
+    spin_result = [symbols.HORSESHOE,
+                   symbols.HORSESHOE,
+                   symbols.STAR]
+
+    result = payout_table.calculate_payout(spin_result)
+
+    assert result == 4
+
+    spin_result = [symbols.HORSESHOE,
+                   symbols.STAR,
+                   symbols.HORSESHOE]
+
+    result = payout_table.calculate_payout(spin_result)
+
+    assert result == 4
+
+    spin_result = [symbols.STAR,
+                   symbols.HORSESHOE,
+                   symbols.HORSESHOE]
+
+    result = payout_table.calculate_payout(spin_result)
+
+    assert result == 4
+
+    # Two horseshoes
+    spin_result = [symbols.HORSESHOE,
+                   symbols.HORSESHOE,
+                   symbols.HORSESHOE]
+
+    result = payout_table.calculate_payout(spin_result)
+
+    assert result == 2
+
+    spin_result = [symbols.HORSESHOE,
+                   symbols.SPADE,
+                   symbols.HORSESHOE]
+
+    result = payout_table.calculate_payout(spin_result)
+
+    assert result == 2

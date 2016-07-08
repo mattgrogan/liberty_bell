a8869d89354ab8acda241e6dee744017b95456f5
Matthew Grogan <mvgnyc@gmail.com>
2016-07-08 15:42:54 -0400

    Added arguments

--- liberty_bell/play.py
+++ liberty_bell/play.py
@@ -1,8 +1,22 @@
 from __future__ import print_function
+import argparse
 from controller import Slot_Game_Controller
 
 if __name__ == '__main__':
 
-    #controller = Slot_Game_Controller(ui="TEXT_UI")
-    #controller = Slot_Game_Controller(ui="GUI_UI")
-    controller = Slot_Game_Controller(ui="RPI_UI")
+    parser = argparse.ArgumentParser(description="Play a slot machine.")
+
+    parser.add_argument("--ui", choices=["text", "gui", "rpi"], required=True)
+
+    args = parser.parse_args()
+
+    if args.ui == "text":
+        ui = "TEXT_UI"
+    elif args.ui == "gui":
+        ui = "GUI_UI"
+    elif args.ui == "rpi":
+        ui = "RPI_UI"
+    else:
+        raise ValueError("Invalid user interface specified.")
+
+    controller = Slot_Game_Controller(ui)

a5382f436273b331cb32e7cb563f4f8a9a769ed0
Matthew Grogan <mvgnyc@gmail.com>
2017-06-06 14:15:37 -0400

    PEP8 indentation

--- liberty_bell/components/gui_1351.py
+++ liberty_bell/components/gui_1351.py
@@ -6,39 +6,41 @@ import PIL.ImageTk as ImageTk
 
 
 class GUI_1351(tk.Label, object):
-  """ This behaves like the SSD1351 Display Adapter """
+    """ This behaves like the SSD1351 Display Adapter """
 
-  def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
 
-    self.width = 128
-    self.height = 128
+        self.width = 128
+        self.height = 128
 
-    self._image = Image.new("RGB", (self.width, self.height), color="#000000")
+        self._image = Image.new(
+            "RGB", (self.width, self.height), color="#000000")
 
-    tk.Label.__init__(self, *args, **kwargs)
+        tk.Label.__init__(self, *args, **kwargs)
 
-    self.update_image()
+        self.update_image()
 
-  def clear(self):
-    self._image = Image.new("RGB", (self.width, self.height), color="#000000")
-    self.update_image()
+    def clear(self):
+        self._image = Image.new(
+            "RGB", (self.width, self.height), color="#000000")
+        self.update_image()
 
-  def update_image(self):
-    """ Write image to the screen """
+    def update_image(self):
+        """ Write image to the screen """
 
-    tkimage = ImageTk.PhotoImage(self._image)
+        tkimage = ImageTk.PhotoImage(self._image)
 
-    self.image = tkimage
-    self.configure(image=tkimage)
-    self.update()
+        self.image = tkimage
+        self.configure(image=tkimage)
+        self.update()
 
-  def write_line(self, line):
+    def write_line(self, line):
 
-    # Offset by -1 to scroll upwards
-    self._image = ImageChops.offset(self._image, 0, -1)
-    pix = self._image.load()
+        # Offset by -1 to scroll upwards
+        self._image = ImageChops.offset(self._image, 0, -1)
+        pix = self._image.load()
 
-    for i, pixel in enumerate(line):
-      pix[i, self.height - 1] = pixel
+        for i, pixel in enumerate(line):
+            pix[i, self.height - 1] = pixel
 
-    self.update_image()
+        self.update_image()

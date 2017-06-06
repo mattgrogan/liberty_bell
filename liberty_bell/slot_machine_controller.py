47d4140b359ccf95df19c1fb9f032af8234a59a1
Matthew Grogan <mvgnyc@gmail.com>
2017-06-05 21:21:47 -0400

    Menu now reverts to parent after trigger

--- liberty_bell/slot_machine_controller.py
+++ liberty_bell/slot_machine_controller.py
@@ -3,209 +3,202 @@ import time
 
 class Buy_Credits_Cmd(object):
 
-  def __init__(self, ui, slot_machine, controller, amount):
-    self.ui = ui
-    self.slot_machine = slot_machine
-    self.controller = controller
-    self.amount = amount
+    def __init__(self, ui, slot_machine, controller, amount):
+        self.ui = ui
+        self.slot_machine = slot_machine
+        self.controller = controller
+        self.amount = amount
 
-  def execute(self, action):
+    def execute(self, action):
 
-    if action == "ACTION_LABEL":
-      message = "Buy %i credit(s)" % self.amount
-      self.ui.menu_display.clear()
-      self.ui.menu_display.add_line(message)
-      self.ui.menu_display.flush()
+        if action == "ACTION_LABEL":
+            message = "Buy %i credit(s)" % self.amount
+            self.ui.menu_display.clear()
+            self.ui.menu_display.add_line(message)
+            self.ui.menu_display.flush()
 
-    if action == "ACTION_DISPLAY":
-      message = "Buying %i Press SPIN" % self.amount
-      self.ui.menu_display.clear()
-      self.ui.menu_display.add_line(message)
-      self.ui.menu_display.flush()
+        if action == "ACTION_DISPLAY":
+            message = "Buying %i Press SPIN" % self.amount
+            self.ui.menu_display.clear()
+            self.ui.menu_display.add_line(message)
+            self.ui.menu_display.flush()
 
-    if action == "ACTION_TRIGGER":
-      self.slot_machine.credits += self.amount
-      self.controller.update_display()
+        if action == "ACTION_TRIGGER":
+            self.slot_machine.credits += self.amount
+            self.controller.update_display()
 
 
 class Toggle_Autoplay_Cmd(object):
 
-  def __init__(self, ui, controller):
-    self.ui = ui
-    self.controller = controller
+    def __init__(self, ui, controller):
+        self.ui = ui
+        self.controller = controller
 
-  def execute(self, action):
-    message = "Autoplay: "
+    def execute(self, action):
+        message = "Autoplay: "
 
-    autoplay = self.controller.options["AUTOPLAY"]
+        autoplay = self.controller.options["AUTOPLAY"]
 
-    if action == "ACTION_LABEL":
-      message += "ON" if autoplay else "OFF"
-      self.ui.menu_display.clear()
-      self.ui.menu_display.add_line(message)
-      self.ui.menu_display.flush()
+        if action == "ACTION_LABEL":
+            message += "ON" if autoplay else "OFF"
+            self.ui.menu_display.clear()
+            self.ui.menu_display.add_line(message)
+            self.ui.menu_display.flush()
 
-    if action == "ACTION_DISPLAY":
-      message = "Press SPIN to Save"
-      self.ui.menu_display.clear()
-      self.ui.menu_display.add_menu_text(message, headline="CONFIRM?")
-      self.ui.menu_display.flush()
+        if action == "ACTION_DISPLAY":
+            message = "Press SPIN to Save"
+            self.ui.menu_display.clear()
+            self.ui.menu_display.add_menu_text(message, headline="CONFIRM?")
+            self.ui.menu_display.flush()
 
-    if action == "ACTION_TRIGGER":
-      self.controller.options["AUTOPLAY"] = not autoplay
+        if action == "ACTION_TRIGGER":
+            # self.controller.menu.navigate(self.controller.root_menu)
+            self.controller.options["AUTOPLAY"] = not autoplay
 
 # TODO: Move this to UI
+
+
 class Update_Display_Cmd(object):
 
-  def __init__(self, ui, text):
-    self.ui = ui
-    self.text = text
+    def __init__(self, ui, text):
+        self.ui = ui
+        self.text = text
 
-  def execute(self, action):
-    print "Received action %s from '%s'" % (action, self.text)
+    def execute(self, action):
+        print "Received action %s from '%s'" % (action, self.text)
 
-    if action == "ACTION_LABEL":
-      self.ui.menu_display.clear()
-      self.ui.menu_display.add_menu_text(self.text)
-      self.ui.menu_display.flush()
+        if action == "ACTION_LABEL":
+            self.ui.menu_display.clear()
+            self.ui.menu_display.add_menu_text(self.text)
+            self.ui.menu_display.flush()
 
 
 class Slot_Machine_Controller(object):
 
-  def __init__(self, slot_machine, ui):
+    def __init__(self, slot_machine, ui):
 
-    self.ui = ui
-    self.slot_machine = slot_machine
-    self.options = {}
-    self.options["AUTOPLAY"] = False
+        self.ui = ui
+        self.slot_machine = slot_machine
+        self.options = {}
+        self.options["AUTOPLAY"] = False
 
-    self.user_opts = []
+        self.user_opts = []
 
-    # Options for purchasing credits
-    self.user_opts.append(Buy_Credits_Cmd(self.ui, self.slot_machine, self, 1))
-    self.user_opts.append(Buy_Credits_Cmd(self.ui, self.slot_machine, self, 10))
-    self.user_opts.append(Buy_Credits_Cmd(self.ui, self.slot_machine, self, 100))
+        # Options for purchasing credits
+        self.user_opts.append(Buy_Credits_Cmd(
+            self.ui, self.slot_machine, self, 1))
+        self.user_opts.append(Buy_Credits_Cmd(
+            self.ui, self.slot_machine, self, 10))
+        self.user_opts.append(Buy_Credits_Cmd(
+            self.ui, self.slot_machine, self, 100))
 
-    self.user_opts.append(Toggle_Autoplay_Cmd(self.ui, self))
+        self.user_opts.append(Toggle_Autoplay_Cmd(self.ui, self))
 
+    @property
+    def name(self):
+        return self.slot_machine.name
 
+    def handle_input(self, command):
 
-  @property
-  def name(self):
-    return self.slot_machine.name
-
-  def get_command(self, command_name, label, params=None):
-
-    if command_name == "BUY_CREDITS":
-      cmd = Buy_Credits_Cmd(self.ui, self.slot_machine, self, params)
-    elif command_name == "TOGGLE_AUTOPLAY":
-      cmd = Toggle_Autoplay_Cmd(self.ui, self)
-    elif command_name == "UPDATE_DISPLAY":
-      cmd = Update_Display_Cmd(self.ui, label)
-
-    return cmd
-
-  def handle_input(self, command):
-
-    if command == "MENU":
-      return self.options
-    if command == "UP":
-      self.slot_machine.increment_bet()
-    if command == "DOWN":
-      self.slot_machine.decrement_bet()
-    if command == "SPIN":
-      if self.slot_machine.can_spin:
-        self.ui.buzzer.button_tone()
-        self.slot_machine.spin()
-
-    self.update_button_state()
-    self.update_display()
-
-    return self
-
-  def update_button_state(self):
-
-    self.ui.menu_button.enabled = not self.slot_machine.is_spinning
-
-    self.ui.spin_button.enabled = self.slot_machine.can_spin
-    self.ui.up_button.enabled = self.slot_machine.can_increase_bet
-    self.ui.down_button.enabled = self.slot_machine.can_decrease_bet
-
-    self.ui.reel1_button.enabled = False
-    self.ui.reel2_button.enabled = False
-    self.ui.reel3_button.enabled = False
-
-  def update_display(self):
-
-    self.ui.menu_display.clear()
-    self.ui.menu_display.add_wrapped_text(self.name)
-    self.ui.menu_display.flush()
-
-    self.ui.credits_led.display(self.slot_machine.credits)
-    self.ui.amount_bet_led.display(self.slot_machine.bet)
-
-    if self.slot_machine.winner_paid > 0:
-      self.ui.winner_paid_led.display(self.slot_machine.winner_paid)
-    else:
-      self.ui.winner_paid_led.clear()
-
-  def start(self):
-    self.update_button_state()
-    self.update_display()
-
-  def stop(self):
-
-    for display in self.ui.reel_displays:
-      display.clear()
-
-    self.ui.amount_bet_led.clear()
-    self.ui.winner_paid_led.clear()
-    self.ui.credits_led.clear()
-
-  def update(self):
-    """ Update one iteration of game play """
-
-    requested_delay_ms = 100
-
-    if self.slot_machine.is_spinning:
-      # Is the animation still running?
-      animation_running = False
-
-      for i, reel in enumerate(self.slot_machine.reels):
-        try:
-          line = reel.next_line()
-          animation_running = True
-          self.ui.reel_displays[i].write_line(line)
-          requested_delay_ms = 0
-        except StopIteration:
-          pass
-
-      if not animation_running:
-        winner = self.slot_machine.eval_spin()
-
-        if winner:
-          # Do a nice animation =)
-          starting_credits = self.slot_machine.prev_credits
-          ending_credits = self.slot_machine.credits
-
-          for i, credits in enumerate(range(starting_credits + 1, ending_credits + 1)):
-            self.ui.credits_led.display(credits)
-            self.ui.winner_paid_led.display(i + 1)
-            self.ui.buzzer.increment_tone()
-            time.sleep(0.10)
-
-        else:
-          self.ui.buzzer.lose_tone()
+        if command == "MENU":
+            return self.options
+        if command == "UP":
+            self.slot_machine.increment_bet()
+        if command == "DOWN":
+            self.slot_machine.decrement_bet()
+        if command == "SPIN":
+            if self.slot_machine.can_spin:
+                self.ui.buzzer.button_tone()
+                self.slot_machine.spin()
 
         self.update_button_state()
         self.update_display()
 
-    else:
-      self.update_button_state()
-      self.update_display()
+        return self
 
-      if self.options["AUTOPLAY"]:
-        # TODO: Give a pause and allow player to enter the menu again
-        self.handle_input("SPIN")
+    def update_button_state(self):
 
-    return requested_delay_ms
+        self.ui.menu_button.enabled = not self.slot_machine.is_spinning
+
+        self.ui.spin_button.enabled = self.slot_machine.can_spin
+        self.ui.up_button.enabled = self.slot_machine.can_increase_bet
+        self.ui.down_button.enabled = self.slot_machine.can_decrease_bet
+
+        self.ui.reel1_button.enabled = False
+        self.ui.reel2_button.enabled = False
+        self.ui.reel3_button.enabled = False
+
+    def update_display(self):
+
+        self.ui.menu_display.clear()
+        self.ui.menu_display.add_wrapped_text(self.name)
+        self.ui.menu_display.flush()
+
+        self.ui.credits_led.display(self.slot_machine.credits)
+        self.ui.amount_bet_led.display(self.slot_machine.bet)
+
+        if self.slot_machine.winner_paid > 0:
+            self.ui.winner_paid_led.display(self.slot_machine.winner_paid)
+        else:
+            self.ui.winner_paid_led.clear()
+
+    def start(self):
+        self.update_button_state()
+        self.update_display()
+
+    def stop(self):
+
+        for display in self.ui.reel_displays:
+            display.clear()
+
+        self.ui.amount_bet_led.clear()
+        self.ui.winner_paid_led.clear()
+        self.ui.credits_led.clear()
+
+    def update(self):
+        """ Update one iteration of game play """
+
+        requested_delay_ms = 100
+
+        if self.slot_machine.is_spinning:
+            # Is the animation still running?
+            animation_running = False
+
+            for i, reel in enumerate(self.slot_machine.reels):
+                try:
+                    line = reel.next_line()
+                    animation_running = True
+                    self.ui.reel_displays[i].write_line(line)
+                    requested_delay_ms = 0
+                except StopIteration:
+                    pass
+
+            if not animation_running:
+                winner = self.slot_machine.eval_spin()
+
+                if winner:
+                    # Do a nice animation =)
+                    starting_credits = self.slot_machine.prev_credits
+                    ending_credits = self.slot_machine.credits
+
+                    for i, credits in enumerate(range(starting_credits + 1, ending_credits + 1)):
+                        self.ui.credits_led.display(credits)
+                        self.ui.winner_paid_led.display(i + 1)
+                        self.ui.buzzer.increment_tone()
+                        time.sleep(0.10)
+
+                else:
+                    self.ui.buzzer.lose_tone()
+
+                self.update_button_state()
+                self.update_display()
+
+        else:
+            self.update_button_state()
+            self.update_display()
+
+            if self.options["AUTOPLAY"]:
+                # TODO: Give a pause and allow player to enter the menu again
+                self.handle_input("SPIN")
+
+        return requested_delay_ms
--- liberty_bell/text_menu/menu_engine.py
+++ liberty_bell/text_menu/menu_engine.py
@@ -1,53 +1,54 @@
 
 
 class Menu_Engine(object):
-  """ Class to handle menu functionality """
+    """ Class to handle menu functionality """
 
-  def __init__(self, initial_item):
-    self.current_item = initial_item
-    self.prev_item = None
-    self.last_invoked_item = None
+    def __init__(self, initial_item):
+        self.current_item = initial_item
+        self.prev_item = None
+        self.last_invoked_item = None
 
-  def navigate(self, target_item):
-    """ Navigate to the target item """
+    def navigate(self, target_item):
+        """ Navigate to the target item """
 
-    if target_item is not None:
-      self.prev_item = self.current_item
-      self.current_item = target_item
-      self.execute_callback_action("ACTION_LABEL")
+        if target_item is not None:
+            self.prev_item = self.current_item
+            self.current_item = target_item
+            self.execute_callback_action("ACTION_LABEL")
 
-  def navigate_to(self, direction):
-    """ Navigate UP, DOWN, PARENT, CHILD  """
+    def navigate_to(self, direction):
+        """ Navigate UP, DOWN, PARENT, CHILD  """
 
-    if direction == "PARENT":
-      self.navigate(self.current_item.parent_item)
-    elif direction == "CHILD":
-      self.navigate(self.current_item.child_item)
-    elif direction == "DOWN":
-      self.navigate(self.current_item.next_item)
-    elif direction == "UP":
-      self.navigate(self.current_item.prev_item)
+        if direction == "PARENT":
+            self.navigate(self.current_item.parent_item)
+        elif direction == "CHILD":
+            self.navigate(self.current_item.child_item)
+        elif direction == "DOWN":
+            self.navigate(self.current_item.next_item)
+        elif direction == "UP":
+            self.navigate(self.current_item.prev_item)
 
-  def invoke(self):
+    def invoke(self):
 
-    prevent_trigger = False
+        prevent_trigger = False
 
-    if self.last_invoked_item != self.current_item:
-      print "Invoking for display"
-      self.last_invoked_item = self.current_item
-      prevent_trigger = True
-      self.execute_callback_action("ACTION_DISPLAY")
+        if self.last_invoked_item != self.current_item:
+            print "Invoking for display"
+            self.last_invoked_item = self.current_item
+            prevent_trigger = True
+            self.execute_callback_action("ACTION_DISPLAY")
 
-    # Navigate to child
-    if self.current_item.child_item is not None:
-      print "Moving to child item"
-      self.navigate(self.current_item.child_item)
-    else:
-      if not prevent_trigger:
-        self.execute_callback_action("ACTION_TRIGGER")
+        # Navigate to child
+        if self.current_item.child_item is not None:
+            print "Moving to child item"
+            self.navigate(self.current_item.child_item)
+        else:
+            if not prevent_trigger:
+                self.execute_callback_action("ACTION_TRIGGER")
+                self.navigate_to("PARENT")
 
-  def execute_callback_action(self, action):
-    self.current_item.execute_callback_action(action)
+    def execute_callback_action(self, action):
+        self.current_item.execute_callback_action(action)
 
-  def render(self):
-    pass
+    def render(self):
+        pass

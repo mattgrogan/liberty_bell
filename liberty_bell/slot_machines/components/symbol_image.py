89d6b22d106523c3279e322be7b3cd3ae74da50b
Matthew Grogan <mvgnyc@gmail.com>
2017-06-06 11:53:03 -0400

    Working on color conversion

--- liberty_bell/slot_machines/components/symbol_image.py
+++ liberty_bell/slot_machines/components/symbol_image.py
@@ -3,50 +3,50 @@ from PIL import Image, ImageOps
 
 class Symbol_Image(object):
 
-  def __init__(self, path, width, height):
+    def __init__(self, path, width, height):
 
-    self.path = path
-    self.width = width
-    self.height = height
+        self.path = path
+        self.width = width
+        self.height = height
 
-    self.image = None
+        self.image = None
 
-    self._current_row = 0  # for the iterations
+        self._current_row = 0  # for the iterations
 
-    self.load_image()
+        self.load_image()
 
-  def load_image(self):
-    """ Load the image from the file """
+    def load_image(self):
+        """ Load the image from the file """
 
-    try:
-      self.image = Image.open(self.path)
-    except:
-      self.image = Image.new("RGB", (self.width, self.height), "#FF0000")
+        try:
+            self.image = Image.open(self.path)
+        except:
+            self.image = Image.new("RGB", (self.width, self.height), "#FF0000")
 
-    # Resize the image if necessary
-    if self.image.size != (self.width, self.height):
-      wb = (self.width - self.image.size[0]) / 2
-      hb = (self.height - self.image.size[1]) / 2
-      border_size = (wb, wb, hb, hb)
-      self.image = ImageOps.expand(self.image, border=border_size)
+        # Resize the image if necessary
+        if self.image.size != (self.width, self.height):
+            wb = (self.width - self.image.size[0]) / 2
+            hb = (self.height - self.image.size[1]) / 2
+            border_size = (wb, wb, hb, hb)
+            self.image = ImageOps.expand(self.image, border=border_size)
 
-    self.image = self.image.convert("RGB")
+        self.image = self.image.convert("RGB")
 
-  def reset(self):
-    self._current_row = 0
+    def reset(self):
+        self._current_row = 0
 
-  def next_line(self):
-    """ Extract a single row from the image """
+    def next_line(self):
+        """ Extract a single row from the image """
 
-    if self._current_row >= self.height:
-      raise StopIteration()
+        if self._current_row >= self.height:
+            raise StopIteration()
 
-    row_data = []
-    pix = self.image.load()
+        row_data = []
+        pix = self.image.load()
 
-    for i in range(self.width):
-      row_data.append(pix[i, self._current_row])
+        for i in range(self.width):
+            row_data.append(pix[i, self._current_row])
 
-    self._current_row += 1
+        self._current_row += 1
 
-    return row_data
+        return row_data
--- liberty_bell/slot_machines/components/symbol_image_w.py
+++ liberty_bell/slot_machines/components/symbol_image_w.py
@@ -5,116 +5,127 @@ from PIL import Image, ImageOps
 
 
 class Symbol_Image_W(object):
-  """ Modification of Symbol_Image that forces a white background """
+    """ Modification of Symbol_Image that forces a white background """
 
-  def __init__(self, path, width, height):
+    def __init__(self, path, width, height):
 
-    self.path = path
-    self.width = width
-    self.height = height
+        self.path = path
+        self.width = width
+        self.height = height
 
-    self.image = None
+        self.image = None
 
-    self._current_row = 0  # for the iterations
+        self._current_row = 0  # for the iterations
 
-    self.load_image()
+        self.load_image()
 
-  def load_image(self):
-    """ Load the image from the file """
+    def load_image(self):
+        """ Load the image from the file """
 
-    #try:
-    png = Image.open(self.path)
-    #except:
-    #  png = Image.new("RGB", (self.width, self.height), "#FF0000")
+        # try:
+        png = Image.open(self.path)
+        # except:
+        #  png = Image.new("RGB", (self.width, self.height), "#FF0000")
 
-    if png.mode == "RGBA":
-      # Fill the alpha channel with white
-      png.load()  # Required for png.split()
-      background = Image.new("RGB", png.size, (255, 255, 255))
-      background.paste(png, mask=png.split()[3])
-      self.image = background
-    else:
-      self.image = png
+        if png.mode == "RGBA":
+            # Fill the alpha channel with white
+            png.load()  # Required for png.split()
+            background = Image.new("RGB", png.size, (255, 255, 255))
+            background.paste(png, mask=png.split()[3])
+            self.image = background
+        else:
+            self.image = png
 
-    w, h = self.image.size
+        w, h = self.image.size
 
-    if w < self.width or h < self.height:
-      # The image is smaller than the screen. How much smaller?
-      w_delta = self.width - w
-      h_delta = self.height - h
+        if w < self.width or h < self.height:
+            # The image is smaller than the screen. How much smaller?
+            w_delta = self.width - w
+            h_delta = self.height - h
 
-      # Find the size of the cardinal directions
-      e = int(math.floor(w_delta / 2))
-      w = int(w_delta - e)
-      n = int(math.floor(h_delta / 2))
-      s = int(h_delta - n)
+            # Find the size of the cardinal directions
+            e = int(math.floor(w_delta / 2))
+            w = int(w_delta - e)
+            n = int(math.floor(h_delta / 2))
+            s = int(h_delta - n)
 
-      border_size = (e, n, w, s)
+            border_size = (e, n, w, s)
 
-      self.image = ImageOps.expand(
-          self.image, border=border_size, fill="#FFFFFF")
-    elif w > self.width or h > self.width:
-      # Image is bigger than the screen. Resize it.
-      self.image.resize((self.width, self.height))
+            self.image = ImageOps.expand(
+                self.image, border=border_size, fill="#FFFFFF")
+        elif w > self.width or h > self.width:
+            # Image is bigger than the screen. Resize it.
+            self.image.resize((self.width, self.height))
 
-    # Remove any alpha channel
-    self.image = self.image.convert("RGB")
+        # Remove any alpha channel
+        self.image = self.image.convert("RGB")
 
-    self.pix = None
+        self.pix = None
 
-  def reset(self):
-    self._current_row = 0
+    def reset(self):
+        self._current_row = 0
 
-  def stash_pixels(self, pixel_converter=None):
-    """ Prepare pixes for screen """
+    def stash_pixels(self, pixel_converter=None):
+        """ Prepare pixes for screen """
 
-    # TODO: Handle pixel conversions for the gui
+        # TODO: Handle pixel conversions for the gui
 
-    pixels = self.image.load()
+        pixels = self.image.load()
 
-    w, h = self.image.size
-    self.pix = [[0 for x in range(w)] for y in range(h)]
+        w, h = self.image.size
+        self.pix = [[0 for x in range(w)] for y in range(h)]
 
-    for y in range(h):
-      for x in range(w):
-        r, g, b = pixels[x, y]
-        self.pix[x][y] = pixel_converter.convert(r, g, b)
+        for y in range(h):
+            for x in range(w):
+                r, g, b = pixels[x, y]
+                self.pix[x][y] = pixel_converter.convert(r, g, b)
 
-  def next_line(self):
-    """ Extract a single row from the image """
+    def next_line(self):
+        """ Extract a single row from the image """
 
-    if self.pix is None:
-      self.stash_pixels(Color565())
+        if self.pix is None:
+            # If RPI UI:
+            # self.stash_pixels(Color565())
+            # If GUI:
+            self.stash_pixels(NullConverter())
 
-    if self._current_row >= self.height:
-      raise StopIteration()
+        if self._current_row >= self.height:
+            raise StopIteration()
 
-    row_data = []
+        row_data = []
 
-    for i in range(self.width):
-      row_data.append(self.pix[i][self._current_row])
+        for i in range(self.width):
+            row_data.append(self.pix[i][self._current_row])
 
-    self._current_row += 1
+        self._current_row += 1
 
-    return row_data
+        return row_data
+
 
 class Color565(object):
-  """ Convert a color to RGB565 """
+    """ Convert a color to RGB565 """
 
-  def convert(self, red, green, blue):
-    # We have 8 bits coming in 0-255
-    # So we truncate the least significant bits
-    # until there's 5 bits for red and blue
-    # and six for green
-    red >>= 3
-    green >>= 2
-    blue >>= 3
+    def convert(self, red, green, blue):
+        # We have 8 bits coming in 0-255
+        # So we truncate the least significant bits
+        # until there's 5 bits for red and blue
+        # and six for green
+        red >>= 3
+        green >>= 2
+        blue >>= 3
 
-    # Now move them to the correct locations
-    red <<= 11
-    green <<= 5
+        # Now move them to the correct locations
+        red <<= 11
+        green <<= 5
 
-    # Then "or" them together
-    result = red | green | blue
+        # Then "or" them together
+        result = red | green | blue
 
-    return result
+        return result
+
+
+class NullConverter(object):
+    """ Do not convert colors """
+
+    def convert(self, red, green, blue):
+        return (red, green, blue)

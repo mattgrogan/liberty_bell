f9046ca55487a0ff53094a72a71249d39b69186c
Matthew Grogan <mvgnyc@gmail.com>
2017-09-01 13:23:34 -0400

    Added box

--- liberty_bell/ui/screen.py
+++ liberty_bell/ui/screen.py
@@ -5,23 +5,46 @@ from liberty_bell.ui.pygame_ui import Reel
 WIN_SIZE = (800, 480)
 
 POS = [(100, 100), (258, 100), (416, 100)]
+
 REEL_SIZE = (128, 300)
+REEL_POS = (100, 100)
+NUMBER_REELS = 3
+REEL_MARGIN = 20
 
 class Liberty_Bell_Screen(object):
 
     def __init__(self):
 
         self._screen = pygame.display.set_mode(WIN_SIZE)
-        self._screen.fill(pygame.Color(255,255,255))
+        self._screen.fill(pygame.Color("slategrey"))
 
         pygame.display.init()
         pygame.display.flip()
 
         self.reels = [None, None, None]
 
-    def set_reel_image(self, reel_number, image):
+        # Set up the reel area
+        reel_box_width = REEL_SIZE[0] * NUMBER_REELS + REEL_MARGIN * (NUMBER_REELS-1)
+        print "width=%i" % reel_box_width
+        reel_box_height = REEL_SIZE[1]
 
-        self.reels[reel_number] = Reel(image, self._screen, POS[reel_number], REEL_SIZE)
+        reel_rect = pygame.Rect(REEL_POS, (reel_box_width, reel_box_height))
+
+        reel_border = self._screen.subsurface(reel_rect.inflate((REEL_MARGIN, REEL_MARGIN)))
+        reel_border.fill(pygame.Color("burlywood3"))
+
+        self._reel_box = self._screen.subsurface(reel_rect)
+        self._reel_box.fill(pygame.Color("white"))
+
+        pygame.display.flip()
+
+
+    def set_reel_image(self, reel_number, image):
+        x, y = REEL_POS
+        w, h = REEL_SIZE
+        x += (w + REEL_MARGIN) * reel_number
+
+        self.reels[reel_number] = Reel(image, self._screen, (x, y), REEL_SIZE)
 
     def flip(self):
 
@@ -36,6 +59,7 @@ class Liberty_Bell_Screen(object):
 
         pygame.display.update(dirty_rects)
 
+    @property
     def is_spinning(self):
         """ Return True if any reels are still spinning """
 
